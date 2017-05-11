fbf45635b3a09c406b835191ceb0e99ef1c6de64
WZTENG <yxzxhywzt@126.com>
2017-05-11 10:41:04 +0800

    修改内容

--- app/src/main/java/com/netease/nim/demo/session/action/RedPackedAction.java
+++ app/src/main/java/com/netease/nim/demo/session/action/RedPackedAction.java
@@ -1,9 +1,12 @@
 package com.netease.nim.demo.session.action;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.widget.Toast;
 
 import com.netease.nim.demo.R;
 import com.netease.nim.demo.file.browser.FileBrowserActivity;
+import com.netease.nim.demo.session.activity.SendRedPackedActivity;
 import com.netease.nim.demo.session.extension.RedPackedAttachment;
 import com.netease.nim.uikit.session.actions.BaseAction;
 import com.netease.nim.uikit.session.constant.RequestCode;
@@ -25,28 +28,37 @@ public class RedPackedAction extends BaseAction {
      * **********************红包************************
      */
     private void sendRedPacked() {
-        FileBrowserActivity.startActivityForResult(getActivity(), makeRequestCode(RequestCode.GET_LOCAL_FILE));
+        SendRedPackedActivity.startActivityForResult(getActivity(), makeRequestCode(100));
     }
 
     @Override
     public void onClick() {
-//        sendRedPacked();
-        RedPackedAttachment redPackedAttachment = new RedPackedAttachment();
-        redPackedAttachment.setFlag((byte) 0);
-        redPackedAttachment.setRedPackedMoney(100);
-        redPackedAttachment.setRedPackedNameLabel("恭喜发财，大吉大利！");
-        redPackedAttachment.setRedPackedFromLabel("藤信红包");
-        IMMessage message = MessageBuilder.createCustomMessage(getAccount(), getSessionType(), "红包", redPackedAttachment);
-        sendMessage(message);
+        sendRedPacked();
+        //测试
+//        RedPackedAttachment redPackedAttachment = new RedPackedAttachment();
+//        redPackedAttachment.setFlag((byte) 0);
+//        redPackedAttachment.setRedPackedMoney(100);
+//        redPackedAttachment.setRedPackedNameLabel("恭喜发财，大吉大利！");
+//        redPackedAttachment.setRedPackedFromLabel("藤信红包");
+//        IMMessage message = MessageBuilder.createCustomMessage(getAccount(), getSessionType(), "红包", redPackedAttachment);
+//        sendMessage(message);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == RequestCode.GET_LOCAL_FILE) {
-            String path = data.getStringExtra(FileBrowserActivity.EXTRA_DATA_PATH);
-            File file = new File(path);
-            IMMessage message = MessageBuilder.createCustomMessage(getAccount(), getSessionType(), "红包", new RedPackedAttachment());
-            sendMessage(message);
+        Toast.makeText(getActivity(), "...", Toast.LENGTH_SHORT).show();
+        if (requestCode == 100) {
+            if (resultCode == Activity.RESULT_OK) {
+                String money = data.getStringExtra("money");
+                String say = data.getStringExtra("say");
+                RedPackedAttachment redPackedAttachment = new RedPackedAttachment();
+                redPackedAttachment.setFlag((byte) 0);
+                redPackedAttachment.setRedPackedMoney(Integer.valueOf(money));
+                redPackedAttachment.setRedPackedNameLabel(say);
+                redPackedAttachment.setRedPackedFromLabel("藤信红包");
+                IMMessage message = MessageBuilder.createCustomMessage(getAccount(), getSessionType(), "红包", redPackedAttachment);
+                sendMessage(message);
+            }
         }
     }
 }
