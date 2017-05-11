a5965851c9585a528ac2500661fbe224c98addea
WZTENG <yxzxhywzt@126.com>
2017-05-11 14:05:48 +0800

    修改内容

--- app/src/main/java/com/netease/nim/demo/wzteng/checkpwd/NumberAdapter.java
+++ app/src/main/java/com/netease/nim/demo/wzteng/checkpwd/NumberAdapter.java
@@ -11,66 +11,66 @@ import android.widget.TextView;
 
 import com.netease.nim.demo.R;
 
-public class NumberAdapter extends BaseAdapter{
+public class NumberAdapter extends BaseAdapter {
 
-	private String mNumbers = "123456789C0#";
-	private Context mContext;
+    private String mNumbers = "123456789C0#";
+    private Context mContext;
 
-	public NumberAdapter(Context mContext) {
-		this.mContext =  mContext;
-	}
-	
-	@Override
-	public int getCount() {
-		return mNumbers.length();
-	}
+    public NumberAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
 
-	@Override
-	public String getItem(int position) {
-		return String.valueOf(mNumbers.charAt(position));
-	}
+    @Override
+    public int getCount() {
+        return mNumbers.length();
+    }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+    @Override
+    public String getItem(int position) {
+        return String.valueOf(mNumbers.charAt(position));
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		ItemHolder itemHolder;
-		if (convertView == null) {
-			itemHolder = new ItemHolder();
-			convertView = LayoutInflater.from(mContext).inflate(R.layout.checkpwd_item_view_input_group_code, null);
-			itemHolder.mRootView = (RelativeLayout) convertView.findViewById(R.id.number_root_view);
-			itemHolder.mNumberTextView = (TextView) convertView.findViewById(R.id.number_textView);
-			itemHolder.mDeleteImageView = (ImageView) convertView.findViewById(R.id.number_delete_imageView);
-			convertView.setTag(itemHolder);
-		} else {
-			itemHolder = (ItemHolder) convertView.getTag();
-		}
-		String curNumber = getItem(position);
-		if ("C".equals(curNumber)) {
-			itemHolder.mDeleteImageView.setVisibility(View.GONE);
-			itemHolder.mNumberTextView.setVisibility(View.VISIBLE);
-			itemHolder.mNumberTextView.setText(curNumber);
-			itemHolder.mRootView.setBackgroundColor(mContext.getResources().getColor(R.color.gray));
-		} else if ("#".equals(curNumber)) {
-			itemHolder.mRootView.setBackgroundColor(mContext.getResources().getColor(R.color.gray));
-			itemHolder.mNumberTextView.setVisibility(View.GONE);
-			itemHolder.mDeleteImageView.setVisibility(View.VISIBLE);
-		} else {
-			itemHolder.mRootView.setBackgroundResource(R.drawable.checkpwd_list_selector);
-			itemHolder.mDeleteImageView.setVisibility(View.GONE);
-			itemHolder.mNumberTextView.setVisibility(View.VISIBLE);
-			itemHolder.mNumberTextView.setText(curNumber);
-		}
-		return convertView;
-	}
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ItemHolder itemHolder;
+        if (convertView == null) {
+            itemHolder = new ItemHolder();
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.checkpwd_item_view_input_group_code, null);
+            itemHolder.mRootView = (RelativeLayout) convertView.findViewById(R.id.number_root_view);
+            itemHolder.mNumberTextView = (TextView) convertView.findViewById(R.id.number_textView);
+            itemHolder.mDeleteImageView = (ImageView) convertView.findViewById(R.id.number_delete_imageView);
+            convertView.setTag(itemHolder);
+        } else {
+            itemHolder = (ItemHolder) convertView.getTag();
+        }
+        String curNumber = getItem(position);
+        if ("C".equals(curNumber)) {
+            itemHolder.mDeleteImageView.setVisibility(View.GONE);
+            itemHolder.mNumberTextView.setVisibility(View.VISIBLE);
+            itemHolder.mNumberTextView.setText(curNumber);
+            itemHolder.mRootView.setBackgroundColor(mContext.getResources().getColor(R.color.gray));
+        } else if ("#".equals(curNumber)) {
+            itemHolder.mRootView.setBackgroundColor(mContext.getResources().getColor(R.color.gray));
+            itemHolder.mNumberTextView.setVisibility(View.GONE);
+            itemHolder.mDeleteImageView.setVisibility(View.VISIBLE);
+        } else {
+            itemHolder.mRootView.setBackgroundResource(R.drawable.checkpwd_list_selector);
+            itemHolder.mDeleteImageView.setVisibility(View.GONE);
+            itemHolder.mNumberTextView.setVisibility(View.VISIBLE);
+            itemHolder.mNumberTextView.setText(curNumber);
+        }
+        return convertView;
+    }
 
 
-	private static class ItemHolder{
-		RelativeLayout mRootView;
-		TextView mNumberTextView;
-		ImageView mDeleteImageView;
-	}
+    private static class ItemHolder {
+        RelativeLayout mRootView;
+        TextView mNumberTextView;
+        ImageView mDeleteImageView;
+    }
 }
--- app/src/main/java/com/netease/nim/demo/wzteng/checkpwd/ToastPopupwindow.java
+++ app/src/main/java/com/netease/nim/demo/wzteng/checkpwd/ToastPopupwindow.java
@@ -15,66 +15,67 @@ import android.widget.TextView;
 import com.netease.nim.demo.R;
 
 /**
- * @author fanbaolong
- * 自己定义的一个toast作用的popupwindow 
- * 主要是为了弹出验证框的时候有时会遮住toast导致不显示，一般情况下直接用myApplication.showToast(); 就行了
+ *         自己定义的一个toast作用的popupwindow
+ *         主要是为了弹出验证框的时候有时会遮住toast导致不显示，一般情况下直接用myApplication.showToast(); 就行了
  */
-public class ToastPopupwindow extends PopupWindow{
+public class ToastPopupwindow extends PopupWindow {
 
-	private View mMenuView;
-	private TextView mToastTv;
+    private View mMenuView;
+    private TextView mToastTv;
 
-	public ToastPopupwindow() {
-	}
+    public ToastPopupwindow() {
+    }
 
 
-	@SuppressLint("InflateParams")
-	public ToastPopupwindow(Context mContext ,View view ,String ss){
-		super(mContext);
-		
-		LayoutInflater inflater = (LayoutInflater) mContext
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		mMenuView = inflater.inflate(R.layout.checkpwd_view_toast, null);
-		
-		mToastTv = (TextView) mMenuView.findViewById(R.id.title_tv);
-		mToastTv.setText(ss);
+    @SuppressLint("InflateParams")
+    public ToastPopupwindow(Context mContext, View view, String ss) {
+        super(mContext);
 
-		//设置ToastPopupwindow的View
-		this.setContentView(mMenuView);
-		//设置ToastPopupwindow弹出窗体的宽
-		this.setWidth(LayoutParams.WRAP_CONTENT);
-		//设置ToastPopupwindow弹出窗体的高
-		this.setHeight(LayoutParams.WRAP_CONTENT);
-		//设置ToastPopupwindow弹出窗体可点击
-		this.setFocusable(false);
-		//设置ToastPopupwindow弹出窗体动画效果
-		this.setAnimationStyle(R.style.AnimBottom);
-		//实例化一个ColorDrawable颜色为半透明
-		ColorDrawable dw = new ColorDrawable(0xb0000000);
-		//设置SelectPicPopupWindow弹出窗体的背景
-		this.setBackgroundDrawable(dw);
-		//设置popup的位置
-		this.showAtLocation(view,
-				Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 10);
-		
-		Message message = handler.obtainMessage(1);     // Message  
-		handler.sendMessageDelayed(message, 1000);  
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mMenuView = inflater.inflate(R.layout.checkpwd_view_toast, null);
 
-	}
-	
-	@SuppressLint("HandlerLeak")
-	Handler handler = new Handler(){
-		public void dispatchMessage(Message msg) {
-			switch (msg.what) {
-			case 1:
-				dismiss();
-				break;
+        mToastTv = (TextView) mMenuView.findViewById(R.id.title_tv);
+        mToastTv.setText(ss);
 
-			default:
-				break;
-			}
-		};
-	};
+        //设置ToastPopupwindow的View
+        this.setContentView(mMenuView);
+        //设置ToastPopupwindow弹出窗体的宽
+        this.setWidth(LayoutParams.WRAP_CONTENT);
+        //设置ToastPopupwindow弹出窗体的高
+        this.setHeight(LayoutParams.WRAP_CONTENT);
+        //设置ToastPopupwindow弹出窗体可点击
+        this.setFocusable(false);
+        //设置ToastPopupwindow弹出窗体动画效果
+        this.setAnimationStyle(R.style.AnimBottom);
+        //实例化一个ColorDrawable颜色为半透明
+        ColorDrawable dw = new ColorDrawable(0xb0000000);
+        //设置SelectPicPopupWindow弹出窗体的背景
+        this.setBackgroundDrawable(dw);
+        //设置popup的位置
+        this.showAtLocation(view,
+                Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 10);
+
+        Message message = handler.obtainMessage(1);     // Message
+        handler.sendMessageDelayed(message, 1000);
+
+    }
+
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler() {
+        public void dispatchMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    dismiss();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        ;
+    };
 
 
 }
