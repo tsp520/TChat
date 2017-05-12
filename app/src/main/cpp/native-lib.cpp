f5d81401cd170d669e6c606d9a2fe8fda7e43163
WZTENG <yxzxhywzt@126.com>
2017-05-12 09:56:52 +0800

    修改内容

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,6 @@ jstring
 Java_com_netease_nim_demo_main_activity_WelcomeActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello by WZTENG";
     return env->NewStringUTF(hello.c_str());
 }
