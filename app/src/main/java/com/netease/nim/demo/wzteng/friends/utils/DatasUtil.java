67dac208379dc4b5e5aa47abb4e84538e580d65c
WZTENG <yxzxhywzt@126.com>
2017-05-16 09:43:06 +0800

    修改内容

--- app/src/main/java/com/netease/nim/demo/wzteng/friends/utils/DatasUtil.java
+++ app/src/main/java/com/netease/nim/demo/wzteng/friends/utils/DatasUtil.java
@@ -162,7 +162,7 @@ public class DatasUtil {
 
             item.setFavorters(createFavortItemList());
             item.setComments(createCommentItemList());
-            int type = getRandomNum(10) % 2;
+            int type = getRandomNum(10) % 3;
             if (type == 0) {
                 item.setType("1");// 链接
                 item.setLinkImg("http://pics.sc.chinaz.com/Files/pic/icons128/2264/%E8%85%BE%E8%AE%AFQQ%E5%9B%BE%E6%A0%87%E4%B8%8B%E8%BD%BD1.png");
@@ -172,8 +172,10 @@ public class DatasUtil {
                 item.setPhotos(createPhotos());
             } else {
                 item.setType("3");// 视频
-                String videoUrl = "http://yiwcicledemo.s.qupai.me/v/80c81c19-7c02-4dee-baca-c97d9bbd6607.mp4";
-                String videoImgUrl = "http://yiwcicledemo.s.qupai.me/v/80c81c19-7c02-4dee-baca-c97d9bbd6607.jpg";
+//                String videoUrl = "http://yiwcicledemo.s.qupai.me/v/80c81c19-7c02-4dee-baca-c97d9bbd6607.mp4";
+//                String videoImgUrl = "http://yiwcicledemo.s.qupai.me/v/80c81c19-7c02-4dee-baca-c97d9bbd6607.jpg";
+                String videoUrl = "http://mirror.aarnet.edu.au/pub/TED-talks/911Mothers_2010W-480p.mp4";
+                String videoImgUrl = "http://pic31.nipic.com/20130624/8821914_104949466000_2.jpg";
                 item.setVideoUrl(videoUrl);
                 item.setVideoImgUrl(videoImgUrl);
             }
